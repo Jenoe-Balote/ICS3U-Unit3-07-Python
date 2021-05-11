3f9dee18c384c9f7f97d2aa5ebb0138bc558cf2d
Ubuntu <ubuntu@ip-172-31-49-223.ec2.internal>
2021-05-11 02:46:25 +0000

    added grandchild.py

--- grandchild.py
+++ grandchild.py
@@ -4,6 +4,7 @@
 # Created on May 2021
 # This program determines if you can date my grandchild
 
+import string
 
 
 def main():
@@ -12,17 +13,21 @@ def main():
     # input
     print("Can you date my grandchild?")
     print("Respond with 1 for yes and 2 for no:")
-    rich = int(input("Are you rich? "))
-    attractive =  int(input("Are you good looking? "))
-    print("")
+    number_rich = str(input("Are you rich? "))
+    number_attractive = str(input("Are you good looking? "))
 
     # process and output
-    if rich == 1 or attractive == 1:
-        print("Congrats! You may date my grandchild!")
-    elif rich == 2 or attractive == 2:
-        print("Stay away from my grandchild!")
-    else:
-        print("What?")
+    try:
+        number_rich = int(number_rich)
+        number_attractive = int(number_attractive)
+        if number_rich == 1 or number_attractive == 1:
+            print("Congrats! You may date my grandchild!")
+        elif number_rich == 2 or number_attractive == 2: 
+            print("Stay away from my grandchild!")
+    except Exception:
+        print("{} is invalid data.".format(number_rich and number_attractive))
+    finally:
+        print("Thanks for playing!")
 
 
 if __name__ == "__main__":
